fa0210ff132119b3fbf313a37a97ced2537c4325
ArshInderSingh <98069169+ArshInderSingh@users.noreply.github.com>
2022-04-11 14:02:14 -0400

    7lab5

---
+++ powershell/Modules/arshinder/arshinder.psm1
@@ -0,0 +1,117 @@
+﻿function get-hardwareinfo {
+Write-Host "System Hardware Description:"
+get-CimInstance -ClassName win32_computersystem | fl
+}
+
+function get-osdetails {
+Write-Host "operating system description:"
+get-CimInstance -ClassName win32_operatingsystem | fl Name, Version
+}
+
+function get-processor {
+Write-Host "Processor Info:"
+get-wmiObject -class Win32_Processor | fl Description, MaxClockSpeed, NumberOfCores, @{n="L1CacheSize";e={switch($_.L1CacheSize){$null{$state="data unavailable"}};$state}}, L2CacheSize, L3CacheSize
+}
+
+function get-memories {
+Write-Host "RAM INFORMATION:"
+$capacityINIT = 0
+get-wmiObject -class Win32_Physicalmemory |
+foreach {
+new-object -TypeName psobject -Property @{
+Manufacturer = $_.manufacturer
+Description = $_.Description
+"Size(GB)" = $_.capacity/1gb
+Bank = $_.banklabel
+Slot = $_.devicelocator
+}
+$capacityINIT += $_.capacity/1gb
+} |
+format-table -auto Manufacturer, Description, "Size(GB)", Bank, Slot
+"Total RAM: ${capacityINIT}GB"
+}
+
+function get-mydisks {
+Write-Host "Disks Info:"
+Get-WmiObject -CLASS Win32_DiskDrive |
+? DeviceID -ne $NULL |
+foreach {
+        $drive = $_
+        $drive.GetRelated("Win32_DiskPartition") |
+            foreach {$logicaldisk = $_.GetRelated("win32_LogicalDisk");
+            if($logicaldisk.size) {
+                new-object -TypeName PSobject -Property @{
+                Manufacturer = $drive.Manufacturer
+                DriveLetter = $logicaldisk.DeviceID
+                Model = $drive.Model
+                Size = [string]($logicaldisk.size/1gb -as [int])+"GB"
+                Free =[String]((($logicaldisk.freespace / $logicaldisk.size) * 100) -as [int]) + "%"
+                FreeSpace =[String]($logicaldisk.freespace / 1gb -as [int]) + "GB"
+} |ft -Auto Manufacturer, model, size, Free, FreeSpace
+}
+}
+}
+}
+
+function get-networkdetails {
+Write-Host "Adapter Info:"
+get-ciminstance win32_networkadapterconfiguration |Where-Object {$_.ipenabled -eq "True" } |
+ft Description, Index, IPAddress, IPSubnet,
+@{n="DNSDomain";
+e={switch($_.DNSServerSearchOrder)
+{$null
+    {$state="data unavailable";$state}
+};
+if($null -ne $_.DNSDomain)
+    {$_.DNSDomain}
+  }
+},
+@{n="DNSServerSearchOrder";
+e={switch($_.DNSServerSearchOrder)
+{$null{
+    $state="data unavailable";
+    $state
+    }
+};
+    if($null -ne $_.DNSServerSearchOrder)
+    {$_.DNSServerSearchOrder}
+    }
+  }
+}
+
+function get-gpu {
+Write-Host "Graphics info:"
+$HorizontalDimm=(get-wmiobject -class win32_videocontroller).CurrentHorizontalResolution -as [string]
+$VerticalDimm=(get-wmiobject Win32_Videocontroller).CurrentVerticalresolution -as [String]
+$Bit=(get-wmiobject -class win32_videocontroller).CurrentBitsPerPixel -as [String]
+$sum= $HorizontalDimm + " x " + $VerticalDimm + " and " + $Bit + " bit"
+get-wmiobject win32_videocontroller|
+fl @{n="Video Card Vendor"; e={$_.AdapterCompatibility}}, Description, @{n="Resolution"; e={$sum -as [string]}}
+}
+
+function welcome {
+write-output "Welcome to planet $env:computername Overlord $env:username"
+$now = get-date -format 'HH:MM tt on dddd'
+write-output "It is $now."
+};
+function get-cpuinfo {
+Get-CimInstance -ClassName CIM_Processor | fl Manufacturer, Name, CurrentClockSpeed, MaxClockSpeed, NumberOfCores
+};
+function get-mydisks{
+Get-PhysicalDisk | ft Manufacturer, Model, SerialNumber, FirmwareVersion, Size
+};
+
+function arshinder-System {
+get-processor
+get-osdetails
+get-memories
+get-gpu
+}
+
+function arshinder-Disks {
+get-mydisks
+}
+
+function arshinder-Network {
+get-networkdetails
+}
---
+++ powershell/lab5.ps1
@@ -0,0 +1,18 @@
+﻿Param ( [switch]$System,[switch]$Disks,[switch]$Network)
+
+import-module arshinder
+
+if ($System -eq $false -and $Disks -eq $false -and $Network -eq $false) {
+   write-host "Script starts here."
+   write-host "Add an arguments to get specific results. Press ctrl+space to get the options"
+   write-host "-----------------------------------------------------------------------------"
+   arshinder-System; arshinder-Disks; arshinder-Network;
+   write-host "Script finishes here."
+} else{
+   if ($System) {
+       arshinder-System   }
+   if ($Disks) {
+       arshinder-Disks    }
+   if ($Network) {
+       arshinder-Network  }
+}
